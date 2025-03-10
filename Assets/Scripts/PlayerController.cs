8791951b2287e1b3d9244642b17ada81e6ed285e
chadoxyz <chadoxyz@gmail.com>
2025-03-09 21:04:19 -0400

    Basic combat additions
    Added attacks in all directions
    Basic enemy properties
    Functionality needs work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,123 +1,123 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.InputSystem;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.InputSystem;
 
-// Takes and handles input and movement for a player character
-public class PlayerController : MonoBehaviour
-{
-    public float moveSpeed = 1f;
-    public float collisionOffset = 0.05f;
-    public ContactFilter2D movementFilter;
-    public SwordAttack swordAttack;
+// // Takes and handles input and movement for a player character
+// public class PlayerController : MonoBehaviour
+// {
+//     public float moveSpeed = 1f;
+//     public float collisionOffset = 0.05f;
+//     public ContactFilter2D movementFilter;
+//     public SwordAttack swordAttack;
 
-    Vector2 movementInput;
-    SpriteRenderer spriteRenderer;
-    Rigidbody2D rb;
-    Animator animator;
-    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+//     Vector2 movementInput;
+//     SpriteRenderer spriteRenderer;
+//     Rigidbody2D rb;
+//     Animator animator;
+//     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
-    bool canMove = true;
+//     bool canMove = true;
 
-    // Start is called before the first frame update
-void Start()
-{
-    rb = GetComponent<Rigidbody2D>();
-    animator = GetComponent<Animator>();
-    spriteRenderer = GetComponent<SpriteRenderer>();
+//     // Start is called before the first frame update
+// void Start()
+// {
+//     rb = GetComponent<Rigidbody2D>();
+//     animator = GetComponent<Animator>();
+//     spriteRenderer = GetComponent<SpriteRenderer>();
 
-    // Try to find SwordAttack automatically
-    if (swordAttack == null)
-    {
-        swordAttack = GetComponentInChildren<SwordAttack>(); // Looks for SwordAttack in Player’s children
+//     // Try to find SwordAttack automatically
+//     if (swordAttack == null)
+//     {
+//         swordAttack = GetComponentInChildren<SwordAttack>(); // Looks for SwordAttack in Player’s children
 
-        if (swordAttack == null)
-        {
-            Debug.LogError("SwordAttack component is missing! Ensure it's a child of the player.");
-        }
-    }
-}
+//         if (swordAttack == null)
+//         {
+//             Debug.LogError("SwordAttack component is missing! Ensure it's a child of the player.");
+//         }
+//     }
+// }
 
 
-    private void FixedUpdate() {
-        if(canMove) {
-            // If movement input is not 0, try to move
-            if(movementInput != Vector2.zero){
+//     private void FixedUpdate() {
+//         if(canMove) {
+//             // If movement input is not 0, try to move
+//             if(movementInput != Vector2.zero){
                 
-                bool success = TryMove(movementInput);
+//                 bool success = TryMove(movementInput);
 
-                if(!success) {
-                    success = TryMove(new Vector2(movementInput.x, 0));
-                }
+//                 if(!success) {
+//                     success = TryMove(new Vector2(movementInput.x, 0));
+//                 }
 
-                if(!success) {
-                    success = TryMove(new Vector2(0, movementInput.y));
-                }
+//                 if(!success) {
+//                     success = TryMove(new Vector2(0, movementInput.y));
+//                 }
                 
-                animator.SetBool("isMoving", success);
-            } else {
-                animator.SetBool("isMoving", false);
-            }
+//                 animator.SetBool("isMoving", success);
+//             } else {
+//                 animator.SetBool("isMoving", false);
+//             }
 
-            // Set direction of sprite to movement direction
-            if(movementInput.x < 0) {
-                spriteRenderer.flipX = true;
-            } else if (movementInput.x > 0) {
-                spriteRenderer.flipX = false;
-            }
-        }
-    }
+//             // Set direction of sprite to movement direction
+//             if(movementInput.x < 0) {
+//                 spriteRenderer.flipX = true;
+//             } else if (movementInput.x > 0) {
+//                 spriteRenderer.flipX = false;
+//             }
+//         }
+//     }
 
-    private bool TryMove(Vector2 direction) {
-        if(direction != Vector2.zero) {
-            // Check for potential collisions
-            int count = rb.Cast(
-                direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
-                movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
-                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
-                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
+//     private bool TryMove(Vector2 direction) {
+//         if(direction != Vector2.zero) {
+//             // Check for potential collisions
+//             int count = rb.Cast(
+//                 direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+//                 movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
+//                 castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+//                 moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
 
-            if(count == 0){
-                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            // Can't move if there's no direction to move in
-            return false;
-        }
+//             if(count == 0){
+//                 rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+//                 return true;
+//             } else {
+//                 return false;
+//             }
+//         } else {
+//             // Can't move if there's no direction to move in
+//             return false;
+//         }
         
-    }
+//     }
 
-    void OnMove(InputValue movementValue) {
-        movementInput = movementValue.Get<Vector2>();
-    }
+//     void OnMove(InputValue movementValue) {
+//         movementInput = movementValue.Get<Vector2>();
+//     }
 
-    void OnFire() {
-        animator.SetTrigger("swordAttack");
-    }
+//     void OnFire() {
+//         animator.SetTrigger("swordAttack");
+//     }
 
-    public void SwordAttack() {
-        LockMovement();
+//     public void SwordAttack() {
+//         LockMovement();
 
-        if(spriteRenderer.flipX == true){
-            swordAttack.AttackLeft();
-        } else {
-            swordAttack.AttackRight();
-        }
-    }
+//         if(spriteRenderer.flipX == true){
+//             swordAttack.AttackLeft();
+//         } else {
+//             swordAttack.AttackRight();
+//         }
+//     }
 
-    public void EndSwordAttack() {
-        UnlockMovement();
-        swordAttack.StopAttack();
-    }
+//     public void EndSwordAttack() {
+//         UnlockMovement();
+//         swordAttack.StopAttack();
+//     }
 
-    public void LockMovement() {
-        canMove = false;
-    }
+//     public void LockMovement() {
+//         canMove = false;
+//     }
 
-    public void UnlockMovement() {
-        canMove = true;
-    }
-}
+//     public void UnlockMovement() {
+//         canMove = true;
+//     }
+// }
